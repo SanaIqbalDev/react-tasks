7878d7c113ed11e0dd6e357c5980ec1812992a21
SanaWebDev <sanaiqbaldev@gmail.com>
2023-09-20 13:14:55 +0500

    deleted unnecessary file

--- .history/src/components/Statistics/ActivityMetrics_20230912163831.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ActivityMetrics = () => {
-    return (
-        <div>ActivityMetrics</div>
-    )
-}
-
-export default ActivityMetrics
--- .history/src/components/Statistics/Statistics_20230907141949.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Statistics = () => {
-    return (
-        <div>I am not Statistics</div>
-    )
-}
-
-export default Statistics
